5c0f9912d88ff79039940faee0e307f1a7e60927
irinapichalka <irinarescan@gmail.com>
2022-10-31 10:10:05 +0200

    test

--- lesson1/task-list/src/scripts/__tests__/todolist.spec.js
+++ lesson1/task-list/src/scripts/__tests__/todolist.spec.js
@@ -1,7 +1,7 @@
 import { compareTasks } from "../render";
 
 it("should compare tasks", () => {
-  const a = 1;
-  const b = 2;
+  const a = new Date("December 17, 1995 03:24:00");
+  const b = new Date("December 17, 1995 04:24:00");
   expect(compareTasks(a, b)).toEqual(a < b);
 });
