ca474f9207a9c337152f7849c5dba463a53eccf4
irinapichalka <97957530+irinapichalka@users.noreply.github.com>
2022-11-02 10:22:51 +0200

    Update webpack.config.js

--- lesson5/task2/webpack.config.js
+++ lesson5/task2/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: {
     profile: "./src/profile/index.js",
-    dasboard: "./src/dashboard/index.js",
+    dashboard: "./src/dashboard/index.js",
   },
   output: {
     filename: "[name].js",
